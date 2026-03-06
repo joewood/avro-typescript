c8bef91ede4aad328225f228246bbcc47f951c38
Sam Sonne <sam.sonne@dat.com>
2026-03-06 14:27:28 -0800

    Implement wrapUnions
    test
    
    Indentation
    
    No newlines
    
    remove unused import
    
    Support namespaces
    
    single-key union record types
    
    Inline namespace const
    
    Support auto union wrapping
    
    Update tests for auto-wrapping
    
    Wrapping is all-or-nothing
    
    avsc treats bytes and fixed the same for ambiguity
    
    Add comments, and special case for null

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ import {
     isLogicalType,
     isMapType,
     isRecordType,
+    NameOrType,
     RecordType,
     Schema,
     Type,
@@ -49,16 +50,21 @@ export function convertPrimitive(avroType: string): string {
 export function avroToTypeScript(schema: Schema, opts: ConversionOptions = {}): string {
     const output: string[] = [];
     if (isEnumType(schema)) convertEnum(schema, output);
-    else if (isRecordType(schema)) convertRecord(schema, output, opts);
+    else if (isRecordType(schema)) convertRecord(schema, output, opts, schema.namespace);
     else throw "Unknown top level type " + (schema as unknown)["type"];
     return output.join("\n");
 }
 
 /** Convert an Avro Record type. Return the name, but add the definition to the file */
-export function convertRecord(recordType: RecordType, fileBuffer: string[], opts: ConversionOptions): string {
+export function convertRecord(
+    recordType: RecordType,
+    fileBuffer: string[],
+    opts: ConversionOptions,
+    namespace: string | undefined,
+): string {
     let buffer = `export interface ${recordType.name} {\n`;
     for (let field of recordType.fields) {
-        buffer += convertFieldDec(field, fileBuffer, opts) + "\n";
+        buffer += convertFieldDec(field, fileBuffer, opts, namespace) + "\n";
     }
     buffer += "}\n";
     fileBuffer.push(buffer);
@@ -72,23 +78,28 @@ export function convertEnum(enumType: EnumType, fileBuffer: string[]): string {
     return enumType.name;
 }
 
-export function convertType(type: Type, buffer: string[], opts: ConversionOptions): string {
+export function convertType(
+    type: Type,
+    buffer: string[],
+    opts: ConversionOptions,
+    namespace: string | undefined,
+): string {
     // if it's just a name, then use that
     if (typeof type === "string") {
         return convertPrimitive(type) || type;
     } else if (type instanceof Array) {
         // array means a Union. Use the names and call recursively
-        return type.map((t) => convertType(t, buffer, opts)).join(" | ");
+        return convertUnion(type, buffer, opts, namespace);
     } else if (isRecordType(type)) {
         //} type)) {
         // record, use the name and add to the buffer
-        return convertRecord(type, buffer, opts);
+        return convertRecord(type, buffer, opts, namespace);
     } else if (isArrayType(type)) {
         // array, call recursively for the array element type
-        return convertType(type.items, buffer, opts) + "[]";
+        return convertType(type.items, buffer, opts, namespace) + "[]";
     } else if (isMapType(type)) {
         // Dictionary of types, string as key
-        return `{ [index:string]:${convertType(type.values, buffer, opts)} }`;
+        return `{ [index:string]:${convertType(type.values, buffer, opts, namespace)} }`;
     } else if (isEnumType(type)) {
         // array, call recursively for the array element type
         return convertEnum(type, buffer);
@@ -96,7 +107,7 @@ export function convertType(type: Type, buffer: string[], opts: ConversionOption
         if (opts.logicalTypes && opts.logicalTypes[type.logicalType]) {
             return opts.logicalTypes[type.logicalType];
         }
-        return convertType(type.type, buffer, opts);
+        return convertType(type.type, buffer, opts, namespace);
     } else if (isFixedType(type)) {
         return "Uint8Array";
     } else {
@@ -105,7 +116,105 @@ export function convertType(type: Type, buffer: string[], opts: ConversionOption
     }
 }
 
-export function convertFieldDec(field: Field, buffer: string[], opts: ConversionOptions): string {
-    let typeName = convertType(field.type, buffer, opts);
+export function convertUnion(
+    type: NameOrType[],
+    buffer: string[],
+    opts: ConversionOptions,
+    namespace: string | undefined,
+): string {
+    // Wrapped unions is all-or-nothing, even if some values are unambiguous within the union.
+    // For example, with "int" | "double" | "string", the numbers are ambigous but the string
+    // should be unambiguous. Nevertheless, at least with avsc it treats ambiguity as
+    // all-or-nothing.
+    if (shouldWrapUnionMembers(type, opts)) {
+        return type
+            .map((t) => {
+                // Null is a special case. Even when using wrapped unions, plain null is allowed
+                // as a value in addition to {"null": null}.
+                const nullPrefix = t === "null" ? "null | " : "";
+                return `${nullPrefix}{ "${keyForUnionElement(t, namespace)}": ${convertType(t, buffer, opts, namespace)} }`;
+            })
+            .join(" | ");
+    } else {
+        return type.map((t) => convertType(t, buffer, opts, namespace)).join(" | ");
+    }
+}
+
+function shouldWrapUnionMembers(type: NameOrType[], opts: ConversionOptions): boolean {
+    switch (opts.wrapUnions) {
+        case "always":
+            return true;
+        case "never":
+            return false;
+        case "auto": {
+            return hasAmbiguousUnionMembers(type);
+        }
+        default:
+            return opts.wrapUnions ?? false;
+    }
+}
+
+function hasAmbiguousUnionMembers(type: NameOrType[]): boolean {
+    // If there is more than one union member that shares any JS type, it'll be considered
+    // ambiguous and must be wrapped.
+    const result: Record<string, boolean> = {};
+    for (const t of type) {
+        const typeName = jsTypeForAvroType(t);
+        if (result[typeName]) {
+            return true;
+        }
+        result[typeName] = true;
+    }
+    return false;
+}
+
+function jsTypeForAvroType(t: NameOrType): string {
+    const avroType = typeof t === "string" ? t : t.type;
+    switch (avroType) {
+        case "long":
+        case "int":
+        case "double":
+        case "float":
+            return "number";
+        case "bytes":
+        case "fixed":
+            return "buffer";
+        case "null":
+            return "null";
+        case "boolean":
+            return "boolean";
+        case "string":
+        case "enum":
+            return "string";
+        case "record":
+        case "map":
+            return "object";
+        case "array":
+            return "array";
+        default:
+            return null;
+    }
+}
+
+export function keyForUnionElement(type: NameOrType, namespace: string | undefined): string {
+    if (typeof type === "string") {
+        return type;
+    }
+    if (isRecordType(type) || isFixedType(type) || isEnumType(type)) {
+        if (namespace) {
+            return `${namespace}.${type.name}`;
+        }
+        return type.name;
+    }
+    return type.type;
+}
+
+export function convertFieldDec(
+    field: Field,
+    buffer: string[],
+    opts: ConversionOptions,
+    namespace: string | undefined,
+): string {
+    let typeName = convertType(field.type, buffer, opts, namespace);
     return `\t${field.name}: ${typeName};`;
 }
